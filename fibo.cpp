1eb5374367b60d7050e72e190e38eb056a567bea
Toufiqul <fahim.prime@gmail.com>
2023-12-25 10:48:22 +0600

    fibo memoized

--- fibo.cpp
+++ fibo.cpp
@@ -6,24 +6,42 @@ int fibo(int n){
     else return (fibo(n-1)+(n-2));              //has segmentfault 
 }
 
+int term[1000];
+// Fibonacci Series using memoized Recursion
+int fibM(int n)
+{
+    // base case
+    if (n <= 1)
+        return n;
+ 
 
-// int main(){
-
-//     int n;
-//     cin>>n;
-//     cout<<fib(n);
-
-//     return 0;
-// }
+    if (term[n] != 0)    // if fib(n) has already been computed we do not do 
+    return term[n];      //further recursive calls and hence reduce the number of repeated work
+ 
+    else {
+ 
+        // store the computed value of fib(n)
+        // in an array term at index n to
+        // so that it does not needs to be
+        // precomputed again
+        term[n] = fibM(n - 1) + fibM(n - 2);
+ 
+        return term[n];
+    }
+}
 
 int main() {
-   int x , i=0;
+   int n , i=0;
    cout << "Enter the number of terms of series : ";
-   cin >> x;
+   cin >> n;
    cout << "\nFibonnaci Series : ";
-   while(i < x) {
+   while(i < n) {
       cout << " " << fibo(i);
       i++;
    }
+   cout<<endl;
+
+   cout<<fibM(n);
+   
    return 0;
 }
