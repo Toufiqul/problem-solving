143c48ccec4d19333743ff89691f34b257510e99
Toufiqul <fahim.prime@gmail.com>
2024-05-05 17:57:51 +0600

    working on 724

--- 724_Find_Pivot_Index.cpp
+++ 724_Find_Pivot_Index.cpp
@@ -5,23 +5,37 @@ using namespace std;
 
 int pivotIndex(vector<int> &nums)
 {
-    vector<int> inOrder = {0};
+    vector<int> inOrder;
+    inOrder.push_back(0);
     vector<int> revOrder = {0};
-    // cout << inOrder.size();
+   
+    
+    // if (!nums.empty()) {
+    //     inOrder.push_back(nums[0]); // Initialize inOrder with the first element of nums
+    // }
+
+    // for (int i = 1; i < nums.size(); i++) {
+    //     inOrder.push_back(inOrder.back() + nums[i]); // Add subsequent elements to inOrder
+    // }
+
+
+    // // cout << inOrder.size();
     for (int i = 0; i < nums.size(); i++)
     {
         inOrder.push_back(*inOrder.crbegin() + nums[i]);
         revOrder.push_back(*revOrder.crbegin() + nums[nums.size() - i - 1]);
     }
-    inOrder.erase(inOrder.begin());
-    revOrder.erase(revOrder.begin());
-    for (int i = 0; i < revOrder.size(); i++)
-    {
-        if (inOrder[i] == revOrder[i])
-            return i;
-    }
-    // cout<<endl;
-    // for(int i = 1; i < revOrder.size();i++)cout<<inOrder[i]<< " ";
+    // inOrder.erase(inOrder.begin());
+    // revOrder.erase(revOrder.begin());
+    // for (int i = 0; i < revOrder.size(); i++)
+    // {
+    //     if (inOrder[i] == revOrder[i])
+    //         return i;
+    // }
+    // // cout<<endl;
+    for(int i = 1; i < revOrder.size();i++){cout<<revOrder[i]<< " ";}
+    cout<<endl;
+    for(int i = 1; i < revOrder.size();i++){cout<<inOrder[i]<< " ";}
     return -1;
 }
 
