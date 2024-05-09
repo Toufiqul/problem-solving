e88f411e2791dc1a8b72c999f9d9c85ac12a95c4
Toufiqul <fahim.prime@gmail.com>
2024-05-09 14:53:13 +0600

    2215 find the difference of two arrays

--- 189_Rotate_Array.cpp
+++ 189_Rotate_Array.cpp
@@ -21,23 +21,19 @@ void rotate(vector<int> &nums, int k)
 void rotate2(vector<int> &nums, int k)
 {
     int n = nums.size();
-    k=k % n;
-    auto it = nums.begin();
+    k = k % n;
 
-    for(int i =0;i<n-k;i++){
+    for (int i = 0; i < n - k; i++)
+    {
         nums.push_back(nums[i]);
-        it++;
     }
-    // cout<<*it<<endl;
-    nums.assign(it,nums.end());
-    nums.begin() = it;
-    cout<<nums.size()<<endl;
-    for(int i =0;i<nums.size();i++){ cout<<nums[i]<<" ";}
+
+    nums.erase(nums.begin(), nums.begin() + n - k);
 }
 
 int main()
 {
-    vector<int>nums = {1,2,3,4,5,6,7};
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
     int k = 3;
     //     vector<int>nums = {-1,-100,3,99};
     // int k = 2;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,2 @@
-ndl;
-    // nums.assign(it,it+n);
-    // cout<<nums.size()<<endl;
+<int>nums = {-1,-100,3,99};
+    int k = 2;
