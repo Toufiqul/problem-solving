17fb8423743a6b41da6d829cbfd3e5b98602de30
Toufiqul <fahim.prime@gmail.com>
2024-05-09 15:09:17 +0600

    2215 with binary search

--- 2215_Find_the_Difference_of_Two_Arrays.cpp
+++ 2215_Find_the_Difference_of_Two_Arrays.cpp
@@ -2,27 +2,41 @@
 using namespace std;
 
 vector<vector<int>> findDifference(vector<int> &nums1, vector<int> &nums2)
-{   
+{
     vector<int> unique1, unique2;
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
-    for(int i =0;i<nums1.size();i++){
-        if(find(nums2.begin(),nums2.end(),nums1[i])==nums2.end() && find(unique1.begin(),unique1.end(),nums1[i])==unique1.end())unique1.push_back(nums1[i]);
+    for (int i = 0; i < nums1.size(); i++)
+    {
+        if (!binary_search(nums2.begin(), nums2.end(), nums1[i]) && !binary_search(unique1.begin(), unique1.end(), nums1[i]))
+            unique1.push_back(nums1[i]);
     }
-    for(int i =0;i<nums2.size();i++){
-                if(find(nums1.begin(),nums1.end(),nums2[i])==nums1.end() && find(unique2.begin(),unique2.end(),nums2[i])==unique2.end())unique2.push_back(nums2[i]);
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        if (!binary_search(nums1.begin(), nums1.end(), nums2[i]) && !binary_search(unique2.begin(), unique2.end(), nums2[i]))
+            unique2.push_back(nums2[i]);
     }
 
     vector<vector<int>> ans;
     ans.push_back(unique1);
     ans.push_back(unique2);
 
+    // for (int i = 0; i < unique1.size(); i++)
+    // {
+    //     cout << unique1[i] << " ";
+    // }
+    // cout << endl;
+    // for (int i = 0; i < unique2.size(); i++)
+    // {
+    //     cout << unique2[i] << " ";
+    // }
+
     return ans;
 }
 
 int main()
 {
-    vector<int> nums1 = {1,2,3,3}, nums2 = {1,1,2,2};
-    findDifference(nums1,nums2);
+    vector<int> nums1 = {1, 2, 3}, nums2 = {2, 4, 6};
+    findDifference(nums1, nums2);
     return 0;
 }
