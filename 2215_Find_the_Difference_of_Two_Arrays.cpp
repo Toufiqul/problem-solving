49246c1f636fb9ce5d49c51699886e582b08a796
unknown <fahim.prime@gmail.com>
2024-06-04 22:02:01 +0600

    find differences with hashmap

--- 2215_Find_the_Difference_of_Two_Arrays.cpp
+++ 2215_Find_the_Difference_of_Two_Arrays.cpp
@@ -21,22 +21,60 @@ vector<vector<int>> findDifference(vector<int> &nums1, vector<int> &nums2)
     ans.push_back(unique1);
     ans.push_back(unique2);
 
-    // for (int i = 0; i < unique1.size(); i++)
-    // {
-    //     cout << unique1[i] << " ";
-    // }
-    // cout << endl;
-    // for (int i = 0; i < unique2.size(); i++)
-    // {
-    //     cout << unique2[i] << " ";
-    // }
+    return ans;
+}
+
+vector<vector<int>> findDifference2(vector<int> &nums1, vector<int> &nums2)
+{
+    map<int, int> mp;
+    vector<int> unique1, unique2;
+    for (int i = 0; i < nums1.size(); i++)
+    {
+        // if(mp.find(nums1[i])!=mp.end())
+        // mp[nums1[i]] = 1;
+        // else
+        mp.insert({nums1[i], 1});
+    }
+
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        if (mp.find(nums2[i]) != mp.end() && mp[nums2[i]]!=2)
+            mp[nums2[i]] = 3;
+        else
+            mp.insert({nums2[i], 2});
+    }
+
+    for (auto i : mp)
+    {
+        cout<<i.first<<" "<<i.second<<endl<<endl;
+        if (i.second == 1)
+        {
+            unique1.push_back(i.first);
+        }
+        else if (i.second == 2)
+        {
+            unique2.push_back(i.first);
+        }
+    }
+    vector<vector<int>> ans;
+    ans.push_back(unique1);
+    ans.push_back(unique2);
+    for (int i = 0; i < ans[0].size(); i++)
+    {
+        cout << ans[0][i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < ans[1].size(); i++)
+    {
+        cout << ans[1][i] << " ";
+    }
 
     return ans;
 }
 
 int main()
 {
-    vector<int> nums1 = {1, 2, 3}, nums2 = {2, 4, 6};
-    findDifference(nums1, nums2);
+    vector<int> nums1 = {-80,-15,-81,-28,-61,63,14,-45,-35,-10}, nums2 = {-1,-40,-44,41,10,-43,69,10,2};
+    findDifference2(nums1, nums2);
     return 0;
 }
