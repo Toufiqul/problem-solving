9c411ec40966dfdac28c7a9cfb51cda30d929009
Toufiqul <fahim.prime@gmail.com>
2025-08-03 01:28:33 +0600

    3sum

--- leetcode/3Sum.py
+++ leetcode/3Sum.py
@@ -14,8 +14,28 @@ def binarySearch(arr, target):
 
 def threeSum(nums):
     nums.sort()
-    for i in range(0,len(nums)):
-        for j in range(i+1, len(nums)):
+    n = len(nums)
+    res = []
+    for i in range(n-2):
+        if i >0 and nums[i]== nums[i-1]:
+            continue
+        left= i+1
+        right = n-1
+        while left<right:
+            target = nums[i] + nums[left] + nums[right]
+            if target > 0:
+                right -=1
+            elif target<0:
+                left +=1
+            else:
+                res.append([nums[i],nums[left],nums[right]])
+                while left<right and nums[left]==nums[left+1]:
+                    left +=1
+                while left<right and nums[right]==nums[right-1]:
+                    right -=1
+                left +=1
+                right -=1
+    return res
 
 
-threeSum(23)
+print(threeSum([-1,0,1,2,-1,-4]))
---
+++ test.py
@@ -0,0 +1,4 @@
+arr = [0,1,2,3,4]
+print(len(arr))
+for i in range(len(arr)-2):
+    print(i)
